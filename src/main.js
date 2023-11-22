9cc1288cf500d1301571c612f974558d4885f1cc
vysingsun <vysingsun@gmail.com>
2023-11-22 23:20:09 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// import '././assets/main.css'
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+// import Vue from 'vue';
+// import MyComponent from './MyComponent.vue';
+
+// new Vue({
+//   render: h => h(MyComponent),
+// }).$mount('#app');
+
+// import './assets/css/main.css'
+
+import { createApp } from 'vue'
+import App from './MyComponent.vue'
+
+createApp(App).mount('#app')
