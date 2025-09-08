fdcb269c27e3391b354bf65c5aac7a8c2dbc1b1f
SorThanaroth <sorthanaroth018@gmail.com>
2025-09-08 15:42:18 +0700

    Build my information.

--- src/main.js
+++ src/main.js
@@ -1,20 +1,4 @@
-// import '././assets/main.css'
-
-// import { createApp } from 'vue'
-// import App from './App.vue'
-
-// createApp(App).mount('#app')
-
-// import Vue from 'vue';
-// import MyComponent from './MyComponent.vue';
-
-// new Vue({
-//   render: h => h(MyComponent),
-// }).$mount('#app');
-
-// import './assets/css/main.css'
-
 import { createApp } from 'vue'
-import App from './MyComponent.vue'
+import App from './App.vue'
 
 createApp(App).mount('#app')
