731a30cddc5ecf66f5fccbc89bfa1b7be72b2ac2
SorThanaroth <sorthanaroth018@gmail.com>
2026-02-07 15:07:47 +0700

    update1111666

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  js.configs.recommended, // This includes the "no-unused-vars" rule by default
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      // Ensure it is set to "error"
+      "no-unused-vars": "error", 
+    },
+  },
+];
+
